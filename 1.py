95b4057c452f777ee5426a32bb42c896fb18c5e8
tomtu <125548913@qq.com>
2020-02-19 10:29:50 +0800

    update update

--- 1.py
+++ 1.py
@@ -1,4 +1,17 @@
+import numpy as np
 file1 = open("D2z.txt", "r")
 text = file1.read()
 print(text)
+text1 = text.split()
+print(text1)
+#text2 = [1,2]
+#print(len(text2))
+num = []
+for j in range(0, 200):
+    for k in range(0, 3):
+        if(k == 0):
+            num.append([])
+        num[j].append(text1[3 * j + k])
+print(num)
+
 file1.close()
